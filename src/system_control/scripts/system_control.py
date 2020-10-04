330328ca58c545e9aaecd7adc382a2ff2c274e13
bitsrt <bitsrt>
2020-10-04 23:45:40 +0800

    移除上下电分支控制、简化代码、添加视觉识别服务调取程序和视觉识别服务功能包
    移除上下电分支控制，上下电作为其他功能包部署在AGX_S上、简化代码、添加视觉识别服务功能包

--- src/system_control/scripts/system_control.py
+++ src/system_control/scripts/system_control.py
@@ -3,28 +3,22 @@
 import rospy
 import roslaunch
 from std_msgs.msg import Int8
+from geometry_msgs.msg import Twist
+from v_detection.srv import  v_detection_task
 
+    
 
-
-global power_on
-global  power_off
-power_on=1
-power_off=0
-
-global power_cmd
-# 0-掉电,1-上电
-power_cmd=0
-global  power_state
-# 0-未上电,1-已上电
-power_state=0
-#上下电控制字和状态字
 
 #机器人移动的数据来源切换
 global change_source
 change_source=0
+# 0-表示使用上位机发布的话题指令
+# 1-表示使用机器人自身进行决策所得到的话题指令
 
 global slam_used
 slam_used=0
+# 0-表示自主导航未开启
+# 1表示导航已开启
 
 #控制字和状态字 类
 class cmd_state:
@@ -32,55 +26,24 @@ class cmd_state:
 		self.cmd= 0
 		self.state= 0
 
-def power_cmd_callback(data):    
-    global power_on
-    global  power_off
-    global  power_cmd
-    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    if data.data==1:        
-        power_cmd=power_on
-    else:
-        power_cmd=power_off
-
-#上下电控制电源板通信控制函数
-def  power_fuction(cmd):
-    if  cmd==1:
-        print "上电"
-    if  cmd==0:
-        print "下电"
-
-def mode_cmd_callback(data):
-    mode.cmd=data.data
-            # if data.data==1:
-            #     mode.cmd=1
-            # elif data.data==2:
-            #     mode.cmd=2
-            # elif data.data==3:
-            #     # launch.shutdown()
-            #     mode.cmd=3
-            # elif data.data==4:
-            #     # launch.shutdown()
-            #     mode.cmd=4
-            # elif data.data==5:
-            #     # launch.shutdown()
-            #     mode.cmd=5
-            # elif data.data==6:
-            #     # launch.shutdown()
-            #     mode.cmd=6
-
-    # print "更换模式"
-
-#SLAM_AGX订阅
+#导航AGX订阅
 def hand_control_callback(data):
     global change_source
     if  change_source==0:
         hand_control(data)
     elif change_source==1:
          pass
+
+
+
  #手动或手柄遥控数据解析
 def hand_control(data):
     # cmd_vel=data.x
-    print "解析手动遥控指令"
+    # 将上位机控制站发布的手柄或者面板按键发布的移动话题转析，才能进入话题转速度指令解析包
+    # ，而自主导航时，则选择直通，即自主导航的输出即为ROS cmd_vel，
+
+    vel_pub.publish
+    print "转析话题"
     pass
 
 #模式控制
@@ -89,11 +52,20 @@ def mode_change_fuction():
     if mode.cmd== 0 or mode.cmd==1 or mode.cmd==2 or mode.cmd==3 or mode.cmd==5 or mode.cmd==6:#只要不是自主导航，其他模式下都可以手动遥控
         if change_source==1:
             slam_nav_task(0)
+        elif mode.cmd==5:
+            #调取视觉识别程序
+            v_detection_task("door")
+        print "识别门"
         change_source=0        
     elif mode.cmd==4:
         slam_nav_task(1)
         change_source=1
+
+        
+
     print  mode.cmd
+    mode.state=mode.cmd
+
 
 #自主任务的启停
 def slam_nav_task(switch):
@@ -101,53 +73,50 @@ def slam_nav_task(switch):
     if switch==1:
         launch.start()
         rospy.loginfo("started")
-        type(launch)
+        # type(launch)
     elif switch==0:
         launch.shutdown()
-        slam_used=-1
+        slam_used=0
  
+def mode_cmd_callback(data):
+    mode.cmd=data.data
      
 #订阅接收上位机话题信息
-def subscrib_topic():
-    rospy.Subscriber("/power_cmd", Int8, power_cmd_callback)
-    rospy.Subscriber("/mode_cmd",Int8,mode_cmd_callback)
-    rospy.Subscriber("/turtle1/cmd_vel",Int8,hand_control_callback)#订阅手柄解算发出的话题
+rospy.Subscriber("/mode_cmd",Int8,mode_cmd_callback)
+rospy.Subscriber("/turtle1/cmd_vel",Int8,hand_control_callback)#订阅手柄或者面板按键发出的运动控制话题#话题名称为robot.xml中的<twist_topic>
+vel_pub = rospy.Publisher('cmd_vel',Twist, queue_size = 10)
+v_detection_task = rospy.ServiceProxy('v_detection_task',  v_detection_task)
+
+move_Twist=Twist()#转析上位机控制站话题的消息
 
 
 if __name__ == '__main__':
 
     rospy.init_node('system_control', anonymous=True)
-    subscrib_topic()
+
+    
     mode=cmd_state()
+
     while not rospy.is_shutdown(): 
-            #任务launch启动项
-        if slam_used==0:
-            uuid = roslaunch.rlutil.get_or_generate_uuid(None, True)
-            roslaunch.configure_logging(uuid)
-            launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/controlstation/AGX/src/slam_task/launch/turtle.launch"])
-            slam_used=1
-        elif slam_used==-1:
-            uuid = roslaunch.rlutil.get_or_generate_uuid(None, True)
-            roslaunch.configure_logging(uuid)
-            launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/controlstation/AGX/src/slam_task/launch/turtle.launch"])
-            slam_used=1
-            print "新建"
-            #新建
-        # 上下电状态控制和切换
-        if (power_cmd and (not power_state)):
-            power_fuction(1)
-            power_state=1
-        elif (not power_cmd and (power_state)):
-            power_fuction(0)
-            power_state=0
+
 
         # 控制模式切换
         if mode.cmd !=0:
             if mode.cmd != mode.state:
             #保证相同的指令每次切换时只响应一次
                 mode_change_fuction()
-                mode.state=mode.cmd
-    
-        
 
-        
+
+            #任务launch启动项
+            # 当slam_used==0时代表当前未运行slam自主任务，同时通过
+        if slam_used==0:
+            uuid = roslaunch.rlutil.get_or_generate_uuid(None, True)
+            roslaunch.configure_logging(uuid)
+            launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/controlstation/AGX/src/slam_task/launch/turtle.launch"])
+            slam_used=1
+        # elif slam_used==-1:
+        #     uuid = roslaunch.rlutil.get_or_generate_uuid(None, True)
+        #     roslaunch.configure_logging(uuid)
+        #     launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/controlstation/AGX/src/slam_task/launch/turtle.launch"])
+        #     slam_used=1
+        #     print "新建"
